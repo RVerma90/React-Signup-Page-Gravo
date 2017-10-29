56c30d149f8d3cdb728d7576a999bb1f31d5d400
RVerma90 <irahulverma90@gmail.com>
2017-10-29 18:39:54 +0000

    Gravo base

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Image source={require('./App/Images/space.jpg')} style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Image source={require('./App/Images/logo_small.png')} style={styles.logo} />
+          </View>
+      </Image>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignSelf: 'stretch',
+      width: null,
+      padding: 20,
+    },
+    logoContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+      width: 300,
+      height: 80,
+    }
+});
